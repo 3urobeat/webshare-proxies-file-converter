17919adf5b731e9de65356f993c6fc8f61f297e1
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-03-13 22:48:46 +0100

    Construct skeleton

---
+++ proxyConverter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+'''
+File: proxyConverter.py
+Project: webshare-proxies-file-converter
+Created Date: 13.03.2023 22:18:12
+Author: 3urobeat
+
+Last Modified: 13.03.2023 22:48:21
+Modified By: 3urobeat
+
+Copyright (c) 2023 3urobeat <https://github.com/HerrEurobeat>
+
+Licensed under the MIT license: https://opensource.org/licenses/MIT
+Permission is granted to use, copy, modify, and redistribute the work.
+Full license information available in the project LICENSE file.
+'''
+
+
+# Quick and dirty lil py script to convert the https://webshare.io proxy file to something https://github.com/HerrEurobeat/steam-comment-service-bot understands
+# File must be in the same directory as this script and named "input.txt"
+# Script will create and write to a file called "proxies.txt" so that you can instantly copy it to the folder of your steam-comment-service-bot installation.
+
+
+# Print welcome msg
+print("webshare-proxies-file-converter v1.0 by 3urobeat\n")
+
+
+# Check if input file exists and abort if it doesn't
+print("Checking if file exists...")
+
+
+# Open file
+print("Opening file...")
+
+
+# Convert file
+print("Converting...")
+
+
+# Write to a new file
+print("Creating and writing to a new file...")
+
+
+# End
+print("Done! Exiting...")
